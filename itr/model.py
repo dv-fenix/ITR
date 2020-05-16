081bbf8ee8dc11ef241b6821f406892150342c94
dv-fenix <45421556+dv-fenix@users.noreply.github.com>
2020-05-16 16:49:42 +0530

    Update model.py
    Modified decoder forward path to accomodate training with lm_labels.

--- itr/model.py
+++ itr/model.py
@@ -1,10 +1,10 @@
 from config import preEncDec
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-
 import torch
+from torch import Tensor
 import pytorch_lightning as pl
-
+import copy
 from eng_utils import gen_model_loaders, flat_accuracy, save_model, build_model
 
 
@@ -22,14 +22,67 @@ class itr_Net(pl.LightningModule):
         self.epoch = 0
         self.eval_steps = 0
 
-    def forward(self, encoder_input_ids, decoder_input_ids):
+    def forward(self, encoder_input_ids, decoder_input_ids, testing=False):
+
+        if testing:
+            encoder_hidden_states = self.encoder(encoder_input_ids)[0]
+            logits_tensor = self.decoder(decoder_input_ids, encoder_hidden_states=encoder_hidden_states)
+            return logits_tensor
+        '''
+        encoder_arr, decoder_arr = self.convert_to_list(encoder_input_ids, decoder_input_ids)
         encoder_hidden_states = self.encoder(encoder_input_ids)[0]
         loss, logits = self.decoder(decoder_input_ids,
                                     encoder_hidden_states=encoder_hidden_states,
                                     masked_lm_labels=decoder_input_ids)
+
         self.loss = loss
         self.logits = logits
         return loss, logits
+        '''
+
+        # iteratively add training loss for each decoder_id
+        total_loss = 0
+
+        # array to store word logits
+        final_sent_logits = []
+
+        # loops over each word: lm_labels-> lm_tensor -> decoder outputs[loss, logits]
+        for j in range(len(decoder_arr[0])):
+            label_ids = copy.deepcopy(decoder_arr)  # create a copy of the decoder_arr for masking
+            for i in range(len(decoder_arr)):
+                if j == 0:
+                    continue
+                label_ids[i][j:] = [-100] * (len(label_ids[0]) - j)  # generate label_ids
+            if j == 0:
+                continue
+
+            label_ids = np.array(label_ids)  # convert to array
+            label_tensor = torch.tensor(label_ids).to('cuda')  ##update tensor at each step
+
+            loss, logits = self.decoder(decoder_input_ids,
+                                        encoder_hidden_states=encoder_hidden_states,
+                                        lm_labels=label_tensor)
+
+            logits_arr = logits.cpu().tolist()
+            # iteratively adds word logits
+            final_sent_logits.append(logits_arr)
+            total_loss = total_loss + loss  ##iteratively add loss
+
+        # normalize loss by dividing by total iterations
+        norm_loss = total_loss / len(decoder_arr[0])
+        # convert to tensor[final sentence in array]
+        logits_tensor = torch.FloatTensor(final_sent_logits)
+
+        self.loss = norm_loss
+        self.logits = logits_tensor #dimension: [sequence length - 1, batch_size, sequence_length, vocab_size]
+
+        return norm_loss, logits_tensor
+
+    def convert_to_list(self, encoder_inp, decoder_inp):
+        encoder_arr = encoder_inp.cpu().tolist()
+        decoder_arr = decoder_inp.cpu().tolist()
+
+        return encoder_arr, decoder_arr
 
     def save(self, output):
         save_model(self.encoder, output.encoder)
